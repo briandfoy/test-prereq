facf8ca6c56da374ef19264deb7f635ab2ab565d
brian d foy <brian.d.foy@gmail.com>
2004-08-25 00:57:06 +0000

    * test for talk
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1307 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Prereq.pm
+++ lib/Prereq.pm
@@ -432,7 +432,7 @@ sub _get_from_file
 This source is part of a SourceForge project which always has the
 latest sources in CVS, as well as all of the previous releases.
 
-	https://sourceforge.net/projects/brian-d-foy/
+	http://sourceforge.net/projects/brian-d-foy/
 
 If, for some reason, I disappear from the world, one of the other
 members of the project can shepherd this module appropriately.
